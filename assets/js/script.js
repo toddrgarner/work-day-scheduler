b59485941130abd835a17f6fd6f51424c4e904ce
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-10-04 02:29:53 -0700

    added setInterval section in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 // naming variables //
 var currentDayEl = $("currentDay");
 // arrays below
-var inutEls = $("input");
+var inputEls = $("input");
 var iconEls = $(".savebtn")
+
+var today = moment();
+var todayHour = moment().format("h");
+
+var eventsArray;
+
+// functions //
+
+// initialize page / Loads and Refresh //
+// Retrieves local storage and blocks with whatever is in there //
+function init() {
+    setInterval(function(){
+        currentDayEl.text(moment("dddd, MMMM D, YYYY hh:mm: A"));
+    }, 1000);
+
+    eventStorage();
+    createTimeblocks();
+    updateTimeblocks();
+}
+
